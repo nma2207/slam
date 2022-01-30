6068a68ba4e14cbc80e84f46338f6aa7b51f2855
nma2207 <nma2207@gmail.com>
2022-01-30 23:34:07 +0300

    add onnxruntime library

---
+++ image_process/include/onnx_nextwork.h
@@ -0,0 +1,11 @@
+#ifndef ONNXNEXTWORK_H
+#define ONNXNEXTWORK_H
+
+
+class OnnxNextwork
+{
+public:
+    OnnxNextwork();
+};
+
+#endif // ONNXNEXTWORK_H
---
+++ image_process/src/onnx_nextwork.cpp
@@ -0,0 +1,6 @@
+#include "onnx_nextwork.h"
+
+OnnxNextwork::OnnxNextwork()
+{
+
+}
